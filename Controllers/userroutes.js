1dfe9f86f0298313d73121c349f089a47699bd66
hemapriya <hemapriyasampath@gmail.com>
2018-11-27 14:06:08 -0500

    with empty []

--- Controllers/userroutes.js
+++ Controllers/userroutes.js
@@ -1,16 +1,29 @@
-const Router= require('express').Router();
-//user = require('../Models/user.models.js');
+const Router= require('express').Router(),
+User = require('../Models/user.models.js');
 
  Router.get('/index',(req,res)=>{
-// 	user.find({}).exec((error,users)=>{
-// 		if(error){
-// 			res.send(error);
-// 		}else{
-// 			res.send(users);
-// 		}
-// 	})
-	res.send('this is where the user info is ');
-//})
+	User.find({}).exec((error,users)=>{
+ 		if(error){
+ 			res.send(error);
+ 		}else{
+ 			res.send(users);
+ 		}
+ 	})
+	//res.send('this is where the user info is ');
+
 })
+ Router.post('/new '(req,res)=>{
+ 	let newuser = new User();
+ 	newuser.username = req.body.username;
+ 	newuser.password = req.body.password;
+ 	newuser.isOver21 = req.body.isOver21;
+ 	newuser.save((error,user)=>){
+ 		if(error){
+ 			res.send(error);
+ 		}else{
+ 			res.send(user);
+ 		}
+ 	}
+ })
 
 module.exports = Router;
