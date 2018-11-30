62f92c106bb895e44e629033db3187fa975a38ee
hemapriya <hemapriyasampath@gmail.com>
2018-11-30 12:01:39 -0500

    update

--- Controllers/userroutes.js
+++ Controllers/userroutes.js
@@ -2,28 +2,63 @@ const Router= require('express').Router(),
 User = require('../Models/user.models.js');
 
  Router.get('/index',(req,res)=>{
-	User.find({}).exec((error,users)=>{
- 		if(error){
- 			res.send(error);
- 		}else{
- 			res.send(users);
- 		}
- 	})
-	//res.send('this is where the user info is ');
+		User.find({}).exec((error,users)=>{
+	 		if(error){
+	 			res.send(error);
+	 		}else{
+	 			res.send(users);
+	 		}
+	 	})
+		//res.send('this is where the user info is ');
 
-})
+	})	
+
  Router.post('/new',(req,res)=>{
- 	let newuser = new User();
- 	newuser.username = req.body.username;
- 	newuser.password = req.body.password;
- 	newuser.isOver21 = req.body.isOver21;
- 	newuser.save((error,user)=>{
- 		if(error){
- 			res.send(error);
- 		}else{
- 			res.send(user);
- 		}
- 	})
- })
+	 	let newuser = new User();
+	 	newuser.username = req.body.username;
+	 	newuser.password = req.body.password;
+	 	newuser.isOver21 = req.body.isOver21;
+	 	newuser.save((error,user)=>{
+	 		if(error){
+	 			res.send(error);
+	 		}else{
+	 			res.send(user);
+	 		}
+	 	})
+	})
+
+ Router.put ('/update/:id',(req,res)=>{
+		 	User.findById(req.params.id ,(error,user)=>{
+		 		if(error){
+		 			res.send(error);
+
+		 		}else{
+		 			user.username=req.body.username || user.username;
+		 			user.password = req.body.password || user.password;
+		 			user.isOver21=req.body.isOver21 || user.isOver21;
+		 			user.save((error,user)=>{
+		 				if(error){
+		 					res.send(error);
+		 						}else{
+		 					res.send(user);
+		 						}
+
+		 			})
+		 		}
+		 	})
+	})
+Router.delete ('/delete/:id',(req,res)=>{
+	User.findByIdAndRemove(req.password.id,(error,user)=>{
+			if(error){
+	 			res.send(error);
+	 		}else{
+	 			res.send(user);
+	 		}
+
+		})
+
+
+
+	})
 
 module.exports = Router;
